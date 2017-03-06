a7b367f4cd26a164dda67f985ab5bc43106e735f
Pascal Bihler <pascal@qfs.de>
2017-03-06 12:02:14 +0100

    Revert "Remove last vestiges of the old safaridriver"
    This reverts commit 3f8afe9655d32865cb78ac2b8de2a9fffd31bd25.

--- java/client/test/org/openqa/selenium/safari/SafariDriverTests.java
+++ java/client/test/org/openqa/selenium/safari/SafariDriverTests.java
@@ -33,6 +33,7 @@ import org.openqa.selenium.testing.TestUtilities;
     CleanSessionTest.class,
     CrossDomainTest.class,
     TechnologyPreviewTest.class,
+    WebSocketConnectionTest.class
 })
 public class SafariDriverTests {
 
